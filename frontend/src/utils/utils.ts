e890970d12a1d3bb2a014a09f29f2e49a2ed7305
Omega <web3omega@gmail.com>
2023-01-21 14:14:57 +0100

    Succesful fetching of metadata JSON

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,5 +1,8 @@
 import { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { Connection, Keypair, PublicKey, Signer, SystemProgram, GetProgramAccountsFilter, AccountInfo } from "@solana/web3.js";
+import * as anchor from "@project-serum/anchor";
+
+export const programId = new PublicKey('ASSYU7dde5y5nhpxhxuz76Q3QhMYATLGqND6J8FGhXL9');
 
 export const getAssociatedTokenAddress = async (
   mint: PublicKey,
@@ -31,3 +34,18 @@ export const getMasterAddress = async (mint: PublicKey): Promise<PublicKey> => {
 
   return address;
 }
+
+export const getTraitPDA = (assembledMint: PublicKey, traitCollectionMint: PublicKey) => 
+{
+  const [traitPda] = PublicKey.findProgramAddressSync(
+    [
+        Buffer.from(anchor.utils.bytes.utf8.encode('trait')),
+        assembledMint.toBuffer(),
+        traitCollectionMint.toBuffer(),
+    ],
+    programId
+  );
+
+  return traitPda;
+}
+
--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -4,48 +4,96 @@ import icon_solscan from '../img/icon/solscan.png';
 import empty_nft from '../img/empty.png';
 import { Trait } from '../components/Trait';
 import { Connection, GetProgramAccountsFilter, Keypair, PublicKey } from '@solana/web3.js';
-import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
-import { keypairIdentity, Metaplex, Nft } from "@metaplex-foundation/js";
+import { Metadata, keypairIdentity, Metaplex, Nft } from "@metaplex-foundation/js";
 import { useEffect, useState } from 'react';
 import hashlist from '../hashlist.json';
+import { IDL, NftDressing } from '../idl/nft_dressing';
+import { AnchorProvider, Program, Wallet, web3 } from '@project-serum/anchor';
+import { getTraitPDA, programId } from '../utils/utils';
 
 
 export const Home: React.FC = () => {
     const connection = new Connection('https://rpc.ankr.com/solana_devnet', 'finalized');
+    
+    // Fixed pubkeys for the collections
     const coll_trait_A = new PublicKey('6EmeCycSPwbxVphxRaW2rLH1xprvJrRNkbckMNSETT3G')
     const coll_trait_B = new PublicKey('GEkndwpLQ9xHaoPRenfad5jybgwBCZo1Ue86MXJKYAd8')
     const coll_trait_C = new PublicKey('GAu921sZx1a2gfJrVajsMKppazBpq77QhanEgCCjBjUZ')
+    const coll_assemblies = new PublicKey('6TPns9NVYBNSBE41WiRfVTek3VXCZgGdr64nXMk14YLV')
+
+    const [fetched, setFetched] = useState(false);
 
     const [traitsA, setTraitsA] = useState<Nft[]>([])
     const [traitsB, setTraitsB] = useState<Nft[]>([])
     const [traitsC, setTraitsC] = useState<Nft[]>([])
+    const [assemblies, setAssembled] = useState<Nft[]>([])
 
     const [selectedTraitA, setSelectedTraitA] = useState<Nft>();
     const [selectedTraitB, setSelectedTraitB] = useState<Nft>();
     const [selectedTraitC, setSelectedTraitC] = useState<Nft>();
 
+    const newKeypair = Keypair.generate();
+    const w: Wallet = {
+        payer: newKeypair,
+        // eslint-disable-next-line no-unused-vars
+        signTransaction: function (_tx: web3.Transaction): Promise<web3.Transaction> {
+            throw new Error('Function not implemented.');
+        },
+        // eslint-disable-next-line no-unused-vars
+        signAllTransactions: function (_txs: web3.Transaction[]): Promise<web3.Transaction[]> {
+            throw new Error('Function not implemented.');
+        },
+        publicKey: newKeypair.publicKey,
+    };
+    const provider = new AnchorProvider(connection, w, {
+        preflightCommitment: 'confirmed',
+        commitment: 'processed',
+    });
+    const program = new Program<NftDressing>(IDL, programId, provider);
+    const metaplexFetcher = Metaplex.make(connection, {cluster: 'devnet'})
+    .use(keypairIdentity(newKeypair)) // Mandatory to use a wallet otherwise any find calls fail (why metaplex????)
+
     //const coll_assembly_collection = new PublicKey('')
 
-    const fetchAllNFTs = async (connection: Connection): Promise<Nft[]> => {
+    const fetchTraitsInAssembly = async (assembly: PublicKey) => {
+        const traitAPDA = getTraitPDA(assembly, coll_trait_A);
+        const traitAPDB = getTraitPDA(assembly, coll_trait_B);
+        const traitAPDC = getTraitPDA(assembly, coll_trait_C);
+
+        const token_A = await metaplexFetcher.nfts().findByToken({token: traitAPDA})
+        console.log(token_A)
+    }
+
+    const fetchAllNFTs = async (): Promise<Nft[]> => {
         console.log("Fetching all")
-        const wallet = Keypair.generate();
-        const metaplexFetcher = Metaplex.make(connection, {cluster: 'devnet'})
-            .use(keypairIdentity(wallet)) // Mandatory to use a wallet otherwise any find calls fail (why metaplex????)
+
+
         const mints = hashlist.map(hash => {return new PublicKey(hash)});
-        const output = await metaplexFetcher.nfts().findAllByMintList({mints});
+        const output = await metaplexFetcher.nfts().findAllByMintList({mints}) ;
 
         console.log(JSON.stringify(output))
-        return output as unknown as Nft[];
+
+        const allNfts = await Promise.all(output.map(async nft => {
+            return await metaplexFetcher.nfts().load({metadata: nft as Metadata});
+        }))
+
+        return allNfts as Nft[];
     }
 
     const fetchAllCollections = async () => {
-        const allNfts = await fetchAllNFTs(connection);
+        const allNfts = await fetchAllNFTs();
 
         setTraitsA(allNfts.filter(nft => nft.collection?.address.toString() === coll_trait_A.toString()))
         setTraitsB(allNfts.filter(nft => nft.collection?.address.toString() === coll_trait_B.toString()))
         setTraitsC(allNfts.filter(nft => nft.collection?.address.toString() === coll_trait_C.toString()))
+        const newAssemblies = allNfts.filter(nft => nft.collection?.address.toString() === coll_assemblies.toString())
 
+        console.log(newAssemblies)
+        setAssembled(newAssemblies) 
 
+        fetchTraitsInAssembly(newAssemblies[0].address);
+        
+        setFetched(true);
     }
 
     useEffect( () => {
@@ -68,7 +116,8 @@ export const Home: React.FC = () => {
                     {traitsA.map(trait => {
                         return <Trait nft={trait} />
                     })}
-                    {traitsA.length === 0 && 'Loading... (This takes a long time... Wen helius on devnet?)'}
+                    {!fetched && 'Loading...'}
+                    {fetched && traitsA.length === 0 && 'Found none'}
                 </div>
                 <div className='w-1/3 flex rounded drop-shadow-lg flex-col m-1'>
                     <div className='p-1 flex'>
@@ -80,7 +129,8 @@ export const Home: React.FC = () => {
                     {traitsB.map(trait => {
                         return <Trait nft={trait} />
                     })}
-                    {traitsB.length === 0 && 'Loading... (This takes a long time... Wen helius on devnet?)'}
+                    {!fetched && 'Loading...'}
+                    {fetched && traitsB.length === 0 && 'Found none'}
                 </div>
                 <div className='w-1/3 flex rounded drop-shadow-lg flex-col m-1'>
                     <div className='p-1 flex'>
@@ -93,7 +143,8 @@ export const Home: React.FC = () => {
                     {traitsC.map(trait => {
                         return <Trait nft={trait} />
                     })}
-                    {traitsC.length === 0 && 'Loading... (This takes a long time... Wen helius on devnet?)'}
+                    {!fetched && 'Loading...'}
+                    {fetched && traitsC.length === 0 && 'Found none'}
                 </div>
             </div>
             <div className='w-2/5 m-2 flex justify-center border rounded-lg border-zinc-900 bg-zinc-900'>
@@ -102,8 +153,10 @@ export const Home: React.FC = () => {
                         <p className="m-auto text-2xl">Assembled NFTs</p>
                     </div>
                     <div className='border m-2 p-2 flex rounded border-gray-800 bg-zinc-800 drop-shadow-lg justify-center'>
-                        <img src={empty_nft} />
-                        <p className="p-1 absolute top-1 left-1/2 -translate-x-1/2">NFT Assembly #1</p>
+                        {assemblies.length > 0 && <>
+                            <img className='w-48 h-48' src={assemblies[0].json?.image} alt='assembly'/>
+                            <p className="p-1 absolute top-1 left-1/2 -translate-x-1/2">{assemblies[0].name}</p>
+                        </>}
                     </div>
                     <div className='flex'>
                         <div className='border m-2 p-2 flex rounded border-gray-800 bg-zinc-800 drop-shadow-lg w-1/3'>
